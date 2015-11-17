74c65689b74ec129deb1d900df0c976e098693e5
valentin_crochemore <valentin.crochemore@live.fr>
2015-11-17 17:40:55 +0100

    add layout + extends layout in index.html.twig

--- app/routes.php
+++ app/routes.php
@@ -5,6 +5,6 @@
  * Date: 11/17/15
  * Time: 5:10 PM
  */
-$app->get('/', function() {
-    return "Accueil";
+$app->get('/', function() use ($app) {
+    return $app['twig']->render('index.html.twig');
 });
