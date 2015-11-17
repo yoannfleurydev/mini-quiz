14361d9ec3594bdef5e1990d40a8f261855695ed
valentin_crochemore <valentin.crochemore@live.fr>
2015-11-17 17:14:17 +0100

    add dependencies (twig, doctrine)

---
+++ app/routes.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: valentin
+ * Date: 11/17/15
+ * Time: 5:10 PM
+ */
+$app->get('/', function() {
+    return "Accueil";
+});
--- web/index.php
+++ web/index.php
@@ -4,9 +4,9 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/', function() {
-    return "Accueil";
-});
+require __DIR__.'/../app/config/dev.php';
+require __DIR__.'/../app/app.php';
+require __DIR__.'/../app/routes.php';
 
 $app->run();
 ?>
