586bb237dfdf8cee1d2e2d6d27946bc413af65a8
valentin_crochemore <valentin.crochemore@live.fr>
2015-11-17 16:47:30 +0100

    add index.php

---
+++ web/index.php
@@ -0,0 +1,12 @@
+<?php
+//web/index.php
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->get('/', function() {
+    return "Accueil";
+});
+
+$app->run();
+?>
