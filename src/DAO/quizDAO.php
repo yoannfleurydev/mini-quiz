c42346a8626c1d27caf84cbb35061178c386fda7
Yoann Fleury <yoann.fleury@yahoo.com>
2015-11-18 15:39:00 +0100

    add Quiz POPO and DAO

--- src/DAO/quizDAO.php
+++ src/DAO/quizDAO.php
@@ -5,5 +5,16 @@ namespace Miniquiz\DAO;
 use Miniquiz\Domain\quiz;
 
 class QuizDAO extends DAO {
-    //TODO
+    /**
+     * @param $row La ligne de la base de données en tableau PHP
+     * @return Quiz L'objet Quiz instancié
+     */
+    protected function buildDomainObject($row)
+    {
+        $quiz = new Quiz();
+        $quiz->setQuizId($row['quiz_id']);
+        $quiz->setQuizTitle($row['quiz_title']);
+        $quiz->setQuizUserId($row['quiz_user_id']);
+        return $quiz;
+    }
 }
