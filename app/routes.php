82ce1351422554c7132a455928b62d36a7f23538
valentin_crochemore <valentin.crochemore@live.fr>
2015-11-18 09:14:46 +0100

    add view login

--- app/routes.php
+++ app/routes.php
@@ -1,10 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: valentin
- * Date: 11/17/15
- * Time: 5:10 PM
- */
+
 $app->get('/', function() use ($app) {
+    return $app['twig']->render('index.html.twig');
+});
+
+$app->get('/login', function() use ($app) {
+    return $app['twig']->render('login.html.twig');
+});
+
+$app->get('/signup', function() use ($app) {
     return $app['twig']->render('index.html.twig');
 });
