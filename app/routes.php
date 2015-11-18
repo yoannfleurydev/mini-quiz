a361d9b9ee445a512e8d45a4d64a1ce664021aa2
valentin_crochemore <valentin.crochemore@live.fr>
2015-11-18 10:08:00 +0100

    add bridge path

--- app/routes.php
+++ app/routes.php
@@ -2,19 +2,19 @@
 
 $app->get('/', function() use ($app) {
     return $app['twig']->render('index.html.twig');
-});
+})->bind('home');
 
 $app->get('/login', function() use ($app) {
     return $app['twig']->render('login.html.twig');
-});
+})->bind('login');
 
 $app->get('/signup', function() use ($app) {
     return $app['twig']->render('index.html.twig');
-});
+})->bind('signup');
 
 $app->get('/quiz', function() use ($app) {
     return $app['twig']->render('quiz.html.twig');
-});
+})->bind('quiz');
 
 $app->get('/quiz/{id}', function() use ($app) {
     return $app['twig']->render('quiz_id.html.twig');
