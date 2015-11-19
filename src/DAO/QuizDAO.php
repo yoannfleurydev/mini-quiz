6fe51bd1d9cc6f91a934483c23950bb0967e6965
Yoann Fleury <yoann.fleury@yahoo.com>
2015-11-19 09:47:08 +0100

    (dao) : add find by id function

--- src/DAO/QuizDAO.php
+++ src/DAO/QuizDAO.php
@@ -5,6 +5,17 @@ namespace Miniquiz\DAO;
 use Miniquiz\Domain\quiz;
 
 class QuizDAO extends DAO {
+    public function find($id) {
+        $sql = "SELECT * FROM user WHERE quiz_id=?";
+        $row = $this->getDb()->fetchAssoc($sql, array($id));
+
+        if ($row)
+            return $this->buildDomainObject($row);
+        else
+            throw new \Exception("No quiz matching id " . $id);
+    }
+
+
     /**
      * @param $row La ligne de la base de données en tableau PHP
      * @return Quiz L'objet Quiz instancié
