a208b65a225717e1194b216e7f5e9860f570bb0c
Yoann Fleury <yoann.fleury@yahoo.com>
2015-11-24 11:15:25 +0100

    format : format source code

--- web/index.php
+++ web/index.php
@@ -1,11 +1,12 @@
 <?php
 //web/index.php
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-require __DIR__.'/../app/config/dev.php';
-require __DIR__.'/../app/app.php';
-require __DIR__.'/../app/routes.php';
+require __DIR__ . '/../app/config/dev.php';
+require __DIR__ . '/../app/app.php';
+require __DIR__ . '/../app/routes.php';
+require __DIR__ . '/../app/admin/routes.php';
 
 $app->run();
