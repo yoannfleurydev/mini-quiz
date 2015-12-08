04ef8ac2f13226d569177d2d6131cdba21a1dddc
valentin_crochemore <valentin.crochemore1@etu.univ-rouen.fr>
2015-12-08 13:36:22 +0100

    remove errorHandler in app.php

--- app/app.php
+++ app/app.php
@@ -3,7 +3,6 @@ use Symfony\Component\Debug\ErrorHandler;
 use Symfony\Component\Debug\ExceptionHandler;
 
 // Register global error and exception handlers
-ErrorHandler::register();
 ExceptionHandler::register();
 
 // Register service providers.
