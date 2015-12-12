ec2a1029e1a1ce935599ecaa106fc3540d698c6b
Yoann Fleury <yoann.fleury@yahoo.com>
2015-12-12 16:29:14 +0100

    fix(signup js) : fix issue #3 - input are now showing ticks at the right time

--- web/js/signup.js
+++ web/js/signup.js
@@ -1,3 +1,7 @@
+$("document").ready(function() {
+    $('#statePass, #statePass2').hide();
+});
+
 $("#user_login").keyup(function () {
     $.post("/mini-quiz/web/signup_check_username", {
             user_login: $("#user_login").val()
@@ -18,7 +22,18 @@ $("#user_password, #user_password2").keyup(function () {
 });
 
 function verifyPass() {
-    if ($("#user_password").val() != $("#user_password2").val()) {
+    var userPassword = $("#user_password");
+    var userPassword2 = $("#user_password2");
+    var userPasswordLength = userPassword.val().length;
+    var userPassword2Length = userPassword2.val().length;
+
+    if (userPasswordLength > 0 && userPassword2Length > 0 && userPasswordLength === userPassword2Length) {
+        $('#statePass, #statePass2').toggle();
+    } else if (userPasswordLength <= 0 && userPassword2Length <= 0) {
+        $('#statePass, #statePass2').hide();
+    }
+
+    if (userPassword.val() != userPassword2.val()) {
         $("#statePass, #statePass2").attr('class', 'glyphicon glyphicon-remove form-control-feedback');
     } else {
         $("#statePass, #statePass2").attr('class', 'glyphicon glyphicon-ok form-control-feedback');
