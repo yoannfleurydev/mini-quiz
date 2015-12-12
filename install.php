8edfa88228be0932ce001e65e36e5b1d7e2f939f
Yoann Fleury <yoann.fleury@yahoo.com>
2015-12-12 17:12:18 +0100

    feat(install) : script now create the administrator

--- install.php
+++ install.php
@@ -11,7 +11,7 @@ $composer = "https://getcomposer.org/composer.phar";
 $composerLocalPath = __DIR__ . "/composer.phar";
 $mysqlScriptPath = __DIR__ . "/db/miniquiz.sql";
 
-fprintf(STDOUT, "Bienvenue dans l'installation en ligne de commande du projet Miniquiz\n");
+fprintf(STDOUT, "\nBienvenue dans l'installation en ligne de commande du projet Miniquiz\n\n");
 printMessage("TELECHARGEMENT DE COMPOSER EN COURS");
 
 /***************************************/
@@ -22,30 +22,31 @@ if (download($composer, $composerLocalPath)) {
 
     exec('php composer.phar install');
 } else {
-    fprintf(STDOUT, "Le telechargement de Composer a echoue. Veuillez verifier votre connexion.");
+    fprintf(STDOUT, "Le telechargement de Composer a echoue. Veuillez verifier votre connexion.\n\n");
     exit(EXIT_FAILURE);
 }
 
 /*****************************************/
 /* INITIALISATION DE LA BASE DE DONNEES  */
 /*****************************************/
-if (PHP_OS == 'WINNT') {
-    echo "Adresse de la base de donnees: ";
-    $dbhost = stream_get_line(STDIN, 1024, PHP_EOL);
-    echo "Utilisateur base de donnees: ";
-    $dbuser = stream_get_line(STDIN, 1024, PHP_EOL);
-    echo "Mot de passe de $dbuser: ";
-    $dbpass = stream_get_line(STDIN, 1024, PHP_EOL);
-    echo "Nom de la base de donnees: ";
-    $dbname = stream_get_line(STDIN, 1024, PHP_EOL);
-} else {
-    $dbhost = readline("Adresse de la base de donnees: ");
-    $dbuser = readline("Utilisateur de la base de données: ");
-    $dbpass = readline("Mot de passe de $dbuser: ");
-    $dbname = readline("Nom de la base de données: ");
-}
 
 printMessage("INITIALISATION DE LA BASE DE DONNEES");
+
+if (PHP_OS == 'WINNT') {
+    echo "\nAdresse de la base de donnees: ";
+    $dbhost = stream_get_line(STDIN, 1024, PHP_EOL);
+    echo "\nUtilisateur base de donnees: ";
+    $dbuser = stream_get_line(STDIN, 1024, PHP_EOL);
+    echo "\nMot de passe de $dbuser: ";
+    $dbpass = stream_get_line(STDIN, 1024, PHP_EOL);
+    echo "\nNom de la base de donnees: ";
+    $dbname = stream_get_line(STDIN, 1024, PHP_EOL);
+} else {
+    $dbhost = readline("\nAdresse de la base de donnees: ");
+    $dbuser = readline("\nUtilisateur de la base de données: ");
+    $dbpass = readline("\nMot de passe de $dbuser: ");
+    $dbname = readline("\nNom de la base de données: ");
+}
 
 $mysqlCommand = 'mysql'
     . ' --host=' . $dbhost
@@ -58,36 +59,48 @@ $mysqlCommand = 'mysql'
 $output = shell_exec($mysqlCommand);
 
 printMessage("CREATION DE L'ADMINISTRATEUR DE MINIQUIZ");
-// TODO
+if (PHP_OS == 'WINNT') {
+    echo "\nIdentifiant de connexion de l'administrateur de l'application: ";
+    $admin_login = stream_get_line(STDIN, 1024, PHP_EOL);
+    echo "\nMot de passe de connexion de l'administrateur de l'application: ";
+    $admin_password = stream_get_line(STDIN, 1024, PHP_EOL);
+} else {
+    $admin_login = readline("\nIdentifiant de connexion de l'administrateur de l'application: ");
+    $admin_password = readline("\nMot de passe de connexion de l'administrateur de l'application: ");
+}
 
+$hashed_password = password_hash($admin_password, PASSWORD_BCRYPT, array('cost' => 11));
+
+$mysqlCommand = 'mysql'
+    . ' --host=' . $dbhost
+    . ' --user=' . $dbuser
+    . ' --password=' . $dbpass
+    . ' --database=' . $dbname
+    . ' --verbose'
+    . ' --execute="INSERT INTO mq_user(user_login, user_password, user_access_id) VALUES(\''. $admin_login . '\', \'' .
+    $hashed_password . '\', 1);"';
+
+$output = shell_exec($mysqlCommand);
 
 /* Fonctions */
 function download($url, $path) {
-    # open file to write
     $fp = fopen ($path, 'w+');
-    # start curl
     $ch = curl_init();
     curl_setopt( $ch, CURLOPT_URL, $url );
-    # set return transfer to false
     curl_setopt( $ch, CURLOPT_RETURNTRANSFER, false );
     curl_setopt( $ch, CURLOPT_BINARYTRANSFER, true );
     curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
-    # increase timeout to download big file
     curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 10 );
-    # write data to local file
     curl_setopt( $ch, CURLOPT_FILE, $fp );
-    # execute curl
     curl_exec( $ch );
-    # close curl
     curl_close( $ch );
-    # close local file
     fclose( $fp );
 
     return filesize($path) > 0;
 }
 
 function printMessage($message) {
-    fprintf(STDOUT, "**********************************************************\n");
+    fprintf(STDOUT, "\n**********************************************************\n");
     fprintf(STDOUT, strtoupper($message) . "\n");
-    fprintf(STDOUT, "**********************************************************\n");
+    fprintf(STDOUT, "**********************************************************\n\n");
 }
